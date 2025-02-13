eacf4633a4e963157b90658f75add02a11de2df4
Rishabh <rishabh.prajapati@rapidops.com>
2025-02-13 14:54:26 +0530

    Added comment on validation code

--- user-service/routes/userRoutes.js
+++ user-service/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const {register, login} = require('../controllers/userController')
 const router = express.Router();
-const {registerUserSchema, loginUserSchema, validate} = require('../utils/validation')
-
-router.post('/register', validate(registerUserSchema),register);
-router.post('/login',validate(loginUserSchema), login);
+// const {registerUserSchema, loginUserSchema, validate} = require('../utils/validation')
+// validate(registerUserSchema),
+// validate(loginUserSchema),
+router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
